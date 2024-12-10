a8ece04618cef1951b69a098c1bf1dc3b67d51e7
Cyrus Smith <cyrus.ma.smith@gmail.com>
2024-12-10 23:51:09 +0100

    rename modules for lint order

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,56 @@
-use advent_of_code_2024::day_1;
-use advent_of_code_2024::day_2;
-use advent_of_code_2024::day_3;
-use advent_of_code_2024::day_4;
-use advent_of_code_2024::day_5;
-use advent_of_code_2024::day_6;
-use advent_of_code_2024::day_7;
-use advent_of_code_2024::day_8;
-use advent_of_code_2024::day_9;
+use advent_of_code_2024::day_01;
+use advent_of_code_2024::day_02;
+use advent_of_code_2024::day_03;
+use advent_of_code_2024::day_04;
+use advent_of_code_2024::day_05;
+use advent_of_code_2024::day_06;
+use advent_of_code_2024::day_07;
+use advent_of_code_2024::day_08;
+use advent_of_code_2024::day_09;
+use advent_of_code_2024::day_10;
 
 fn main() {
-    // println!("day 1");
-    // println!("part 1: {}", day_1::part_1());
-    // println!("part 2: {}", day_1::part_2());
+    println!("day 1");
+    println!("part 1: {}", day_01::part_1());
+    println!("part 2: {}", day_01::part_2());
 
-    // println!("day 2");
-    // println!("part 1: {}", day_2::part_1());
-    // println!("part 2: {}", day_2::part_2());
+    println!("day 2");
+    println!("part 1: {}", day_02::part_1());
+    println!("part 2: {}", day_02::part_2());
 
-    // println!("day 3");
-    // println!("part 1: {}", day_3::part_1());
-    // println!("part 2: {}", day_3::part_2());
+    println!("day 3");
+    println!("part 1: {}", day_03::part_1());
+    println!("part 2: {}", day_03::part_2());
 
-    // println!("day 4");
-    // println!("part 1: {}", day_4::part_1());
-    // println!("part 2: {}", day_4::part_2());
+    println!("day 4");
+    println!("part 1: {}", day_04::part_1());
+    println!("part 2: {}", day_04::part_2());
 
-    // println!("day 5");
-    // println!("part 1: {}", day_5::part_1());
-    // println!("part 2: {}", day_5::part_2());
+    println!("day 5");
+    println!("part 1: {}", day_05::part_1());
+    println!("part 2: {}", day_05::part_2());
 
-    // println!("day 6");
-    // println!("part 1: {}", day_6::part_1());
-    // println!("part 2: {}", day_6::part_2());
+    println!("day 6");
+    println!("part 1: {}", day_06::part_1());
+    println!("part 2: {}", day_06::part_2());
 
-    // println!("day 6");
-    // println!("part 1: {}", day_6::part_1());
-    // println!("part 2: {}", day_6::part_2());
+    println!("day 6");
+    println!("part 1: {}", day_06::part_1());
+    println!("part 2: {}", day_06::part_2());
 
-    // println!("day 7");
-    // println!("part 1: {}", day_7::part_1());
-    // println!("part 2: {}", day_7::part_2());
+    println!("day 7");
+    println!("part 1: {}", day_07::part_1());
+    println!("part 2: {}", day_07::part_2());
 
-    // println!("day 8");
-    // println!("part 1: {}", day_8::part_1());
-    // println!("part 2: {}", day_8::part_2());
+    println!("day 8");
+    println!("part 1: {}", day_08::part_1());
+    println!("part 2: {}", day_08::part_2());
 
     println!("day 9");
-    // println!("part 1: {}", day_9::part_1());
-    // println!("part 2: {}", day_9::part_2());
+    println!("part 1: {}", day_09::part_1());
+    println!("part 2: {}", day_09::part_2());
+
+    println!("day 10");
+    println!("part 1: {}", day_10::part_1());
+    println!("part 2: {}", day_10::part_2());
 }
